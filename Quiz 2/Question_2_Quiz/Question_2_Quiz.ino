59975629d02a3ab12b589acd1c40dbe925a5ea4c
David Roman <david.roman@mail.citytech.cuny.edu>
2017-10-12 18:05:37 -0400

    Add files via upload

---
+++ Quiz 2/Question_2_Quiz/Question_2_Quiz.ino
@@ -0,0 +1,15 @@
+int ledPin = 8;
+int buttonPin = 10;
+void setup() {
+pinMode(buttonPin, INPUT_PULLUP);
+Serial.begin(9600);
+}
+void loop() {
+int buttonState = digitalRead(buttonPin);
+if(buttonState == 0) {
+    digitalWrite(ledPin, LOW);
+    delay(500);
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+  }
+}
